f3ee97d04ceb14bb68c026f092500756820151d3
Karl Hiner <karl.hiner@gmail.com>
2022-10-09 15:21:57 -0400

    Billboard scraping is working. Details below.
    - Writing to CSV with configurable `--output` filename.
    - Add `--chart` option for selecting between 'hot-100' and 'billboard-200' charts
    - Add short versions and help text for all options
    - Document output CSV spec in readme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # billboard-chordify-scraper
-Scrape chords for billboard top-200 songs
+Scrape chords for billboard hot-100 songs
 
 ## Install and run
 
@@ -8,7 +8,25 @@ $ pip install -r requirements.txt
 $ python scrape_billboard.py # Defaults to the range [1 year ago, most recent billboard week]
 ```
 
-### Using arguments
+This should create a file called `billboard.csv` with an entry for each song for each week.
+
+### CSV output specification
+
+The output CSV has a header row and a row for each song for each week in the requested period.
+
+Here are the details for each column:
+
+Column name | Column type | Description
+------------- | ------------- | -------------
+date  | string | Chart date for the entry (in "YYYY-mm-dd" format)
+pos  | int | Position in this week's chart
+pos_prev  | int | Position of the song in the previous week's chart. 0 if it was not present in previous week.
+pos_peak  | int | Peak position of the song across all Billboard Hot-100 charts. Always present and non-0.
+artist  | string | Artist name
+song  | string | Song name
+weeks  | int | Number of consecutive weeks on the charts
+
+### Options
 
 Assuming the system time when running is within Sunday Oct 9, 2022, let's request a start-date, `2016-4-25` (which happens to be a Monday) and an explicit date of `2022-10-8"` (yesterday - Saturday). The start and end dates will be rounded to the previous Saturday, which is what Billboard uses for their weekly top-200 chart end-date. Since the requested end date is less than one week ago, results will be limited to the week ending the previous Saturday to ensure the Billboard chart is available:
 
@@ -18,3 +36,21 @@ Requested dates: 2016-04-25 to 2022-10-08
 Converted dates: 2016-04-23 to 2022-10-01
 ```
 
+All option details:
+
+```shell
+$ python scrape_billboard.py --help
+usage: scrape_billboard.py [-h] [-s START] [-e END] [-c CHART] [-o OUTPUT] [-v VERBOSE]
+
+options:
+  -h, --help            show this help message and exit
+  -s START, --start START
+                        Start date in YYYY-mm-dd format. Defaults to one year ago
+  -e END, --end END     End date in YYYY-mm-dd format. Defaults to the most recent saturday with a Billboard chart.
+  -c CHART, --chart CHART
+                        Default to "100" for the "Hot-100" list. Other option is "200" for "Billboard-200.
+  -o OUTPUT, --output OUTPUT
+                        The name of the output CSV file (with a .csv suffix). Defaults to "billboard.csv"
+  -v VERBOSE, --verbose VERBOSE
+                        Verbose logging. Default to true.
+```
--- scrape_billboard.py
+++ scrape_billboard.py
@@ -1,6 +1,7 @@
 import argparse
 from datetime import datetime, date, timedelta
 
+import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
@@ -16,7 +17,7 @@ def validate_date(date_str):
     except ValueError:
         raise ValueError('Incorrect date format. Got: {}. Expected format: YYYY-mm-dd.'.format(date_str))
 
-# Formats the provided `datetime.date` as "yyyy-mm-dd" (the format the billboard top 200 expects in URLs).
+# Formats the provided `datetime.date` as "YYYY-mm-dd" (the format the billboard top 200 expects in URLs).
 def format_ymd(date):
     return date.strftime(DATE_FORMAT)
 
@@ -27,9 +28,16 @@ def prev_saturday(d):
     return limited_d - timedelta(days=((limited_d.weekday() + 2) % 7)) # `weekday()` returns 0 (Mon) - 6 (Sun)
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--start', type=str, required=False, default=(date.today() - ONE_YEAR).strftime(DATE_FORMAT)) # Default to 1 year ago
-parser.add_argument('--end', type=str, required=False, default=date.today().strftime(DATE_FORMAT)) # Defaults to today
-parser.add_argument('--verbose', type=bool, required=False, default=True)
+parser.add_argument('-s', '--start', type=str, required=False, default=(date.today() - ONE_YEAR).strftime(DATE_FORMAT),
+    help='Start date in YYYY-mm-dd format. Defaults to one year ago')
+parser.add_argument('-e', '--end', type=str, required=False, default=date.today().strftime(DATE_FORMAT),
+    help='End date in YYYY-mm-dd format. Defaults to the most recent saturday with a Billboard chart.')
+parser.add_argument('-c', '--chart', type=str, required=False, default='100',
+    help='Default to "100" for the "Hot-100" list. Other option is "200" for "Billboard-200.')
+parser.add_argument('-o', '--output', type=str, required=False, default='billboard.csv',
+    help='The name of the output CSV file (with a .csv suffix). Defaults to "billboard.csv"')
+parser.add_argument('-v', '--verbose', type=bool, required=False, default=True,
+    help='Verbose logging. Default to true.')
 args = parser.parse_args()
 
 start_date_in = validate_date(args.start)
@@ -49,21 +57,62 @@ debug('Converted dates: {} to {}'.format(format_ymd(start_date), format_ymd(end_
 if start_date >= end_date:
     raise ValueError('Start date must be before end date')
 
-# Fetch the Billboard-200 for the week ending on the provided date.
+# Fetch the Billboard-100 for the week ending on the provided date.
 # Returns parsed HTML content.
 # Provided date is expected to be a Saturday.
 # The Billboard site seems to handle in-between dates just fine, but being save since this would indicate something is off in week-iterating.
-def fetch_billboard_200_week(week_end_date):
+# `list` can be either '100' or '200'
+def fetch_billboard_week(week_end_date, chart_type='100'):
+    if '100' not in chart_type and '200' not in chart_type:
+        raise ValueError('Unknown chart type: "{}". Expected "100" or "200".'.format(chart_type))
     if week_end_date.weekday() != 5:
-        raise ValueError('Attempting to fetch a Billboard-200 week using a non-Saturday date: {}'.format(week_end_date))
+        raise ValueError('Attempting to fetch a Billboard week using a non-Saturday date: {}'.format(week_end_date))
 
-    url = 'https://www.billboard.com/charts/billboard-200/{}/'.format(format_ymd(week_end_date))
+    url = 'https://www.billboard.com/charts/{}/{}/'.format('hot-100' if '100' in chart_type else 'billboard-200', format_ymd(week_end_date))
     debug('Fetching: {}'.format(url))
-    content = requests.get(url).content
-    return BeautifulSoup(content, 'html.parser')
+    html = requests.get(url).text
+    return BeautifulSoup(html, 'html.parser')
 
+# Returns a dict with the following attributes:
+# 'date' (string): Chart date for the entry (in "YYYY-mm-dd" format)
+# 'pos' (int): Position in this week's chart
+# 'pos_prev' (int): Position of the song in the previous week's chart. 0 if it was not present in previous week.
+# 'pos_peak' (int): Peak position of the song across all Billboard Hot-100 charts. Always present and non-0.
+# 'artist' (string): Artist name
+# 'song' (string): Song name
+# 'weeks' (int): Number of consecutive weeks on the charts
+def parse_chart_row(chart_row, chart_date):
+    pos = int(chart_row['data-detail-target'])
+    inner = chart_row.find_all('li', recursive=False)[-1]
+    items = inner.find('ul').find_all('li')
+    artist = items[0].find('span').string.strip()
+    song = items[0].find('h3').string.strip()
+    pos_prev = items[3].find('span').string.strip()
+    pos_peak = int(items[4].find('span').string.strip())
+    weeks = int(items[5].find('span').string.strip())
+    return {
+        'date': format_ymd(chart_date),
+        'pos': pos,
+        'pos_prev': 0 if '-' in pos_prev else int(pos_prev),
+        'pos_peak': pos_peak,
+        'artist': artist,
+        'song': song,
+        'weeks': weeks,
+    }
+
+
+all_chart_rows = []
 fetching_date = start_date
 while fetching_date <= end_date:
-    billboard_html = fetch_billboard_200_week(fetching_date)
-    debug(billboard_html)
+    billboard = fetch_billboard_week(fetching_date, args.chart)
+    chart_rows = billboard.find_all('ul', {'class': 'o-chart-results-list-row'})
+    if len(chart_rows) != 100 and len(chart_rows) != 200:
+        raise ValueError('Expected 100/200 chart rows, but found: {}'.format(len(chart_rows)))
+
+    chart_rows_parsed = [parse_chart_row(chart_row, fetching_date) for chart_row in chart_rows]
+    all_chart_rows.extend(chart_rows_parsed)
+
     fetching_date += ONE_WEEK
+
+df = pd.DataFrame(all_chart_rows)
+df.to_csv(args.output, header=True)
