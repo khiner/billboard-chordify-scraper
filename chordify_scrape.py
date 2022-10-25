9608e425209c26cf886ee9b5f3b303b2411d2f65
Karl Hiner <karl.hiner@gmail.com>
2022-10-24 22:42:44 -0400

    Add `-n/--topn` arg to `chordify_scrape` script, which limits chord scraping to the first N songs in each year in the provided CSV
    Also use a dataframe for all processing, without any intermediate dictionary.

--- chordify_scrape.py
+++ chordify_scrape.py
@@ -6,6 +6,7 @@ import re
 import pandas as pd
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions
@@ -20,8 +21,11 @@ parser.add_argument('billboard_csv', nargs='?', type=str, default='billboard_red
                          ' Defaults to "billboard_reduced.csv".')
 parser.add_argument('-o', '--output', type=str, required=False, default='chordify.csv',
                     help='The name of the output CSV file (including the .csv suffix). Defaults to "chordify.csv".')
-parser.add_argument('-v', '--verbose', type=bool, required=False, default=True,
-                    help='Verbose logging. Defaults to true.')
+parser.add_argument('-n', '--topn', type=int, required=False, default=100,
+                    help='The number of songs per year for which to scrape chords, starting at the first song for each year in the provided CSV.'
+                         ' To scrape chords for all songs, just pass a number greater than or equal to the max number of songs in any year.'
+                         ' Defaults to 100.')
+parser.add_argument('-v', '--verbose', type=bool, required=False, default=True, help='Verbose logging. Defaults to true.')
 args = parser.parse_args()
 
 
@@ -36,7 +40,7 @@ if not os.path.isfile(billboard_csv):
     raise ValueError('Input file "{}" not found.'.format(billboard_csv))
 
 log('Loading input file: {}'.format(billboard_csv))
-df = pd.read_csv(billboard_csv)
+df = pd.read_csv(billboard_csv, parse_dates=['date'])
 
 columns = {'date', 'pos', 'pos_prev', 'pos_peak', 'artist', 'song', 'weeks'}
 if set(df.columns) != columns:
@@ -111,33 +115,45 @@ def get_song_data(result_element):
     return key_chord, [chord for chord in song_chords if chord is not None]  # Filter out any `None` values.
 
 
-records = df.to_dict('records')  # This list of dicts is modified, with each item getting new 'key' and 'chords' properties.
-for record in records[:10]:  # todo only trying the first 10 songs for now. Get rid of this limit when things are working better.
-    url = 'https://chordify.net/search/{} {}'.format(record['artist'], record['song'])
-    log('Navigating to {}'.format(url))
-    driver.get(url)
+# This dataframe is modified, with each item getting new 'key' and 'chords' columns.
+# records_by_year = dict(tuple(df.groupby('year')))
+df['year'] = df['date'].dt.year  # Temporary column.
+df['key'] = ''  # Each value will be a chord string
+df['chords'] = ''  # Each value will be a JSON list of chords
 
-    # Wait for at least one result to show up.
-    WebDriverWait(driver, 8).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'section > a')))
-    results_html = driver.page_source
-    result_elements = BeautifulSoup(results_html, 'html.parser').find('section').find_all('a', href=True)
-    for result_index, result in enumerate(result_elements):
-        try:
-            log('Getting result #{} for {}: {}'.format(result_index + 1, record['artist'], record['song']))
-            key, chords = get_song_data(result)
-            log('\tFound key and chords: {}, {}'.format(key, chords))
+for year, records in df.groupby('year'):
+    for record_index, record in records.head(args.topn).iterrows():  # Scrape chords for the top-N songs in each year
+        url = 'https://chordify.net/search/{} {}'.format(record['artist'], record['song'])
+        log('Navigating to {}'.format(url))
+        driver.get(url)
 
-            record['key'] = key
-            record['chords'] = chords
-            break  # We successfully got the data for one of the search results. Move on to the next record.
-        except Exception:
-            logging.exception('Exception getting song data:')
-    if 'key' not in record or 'chords' not in record:
-        log('All {} results tried for {}: {}. Moving to next song without adding columns.'
-            .format(len(result_elements), record['artist'], record['song']))
+        # Wait for at least one result to show up.
+        WebDriverWait(driver, 8).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'section > a')))
+        results_html = driver.page_source
+        result_elements = BeautifulSoup(results_html, 'html.parser').find('section').find_all('a', href=True)
+        for result_index, result in enumerate(result_elements):
+            try:
+                log('Getting result #{} for {}: {}'.format(result_index + 1, record['artist'], record['song']))
+                key, chords = get_song_data(result)
+                log('\tFound key and {} chords.'.format(len(chords)))
 
-df = pd.DataFrame(records)
-df.to_csv(args.output, header=True, index=False)
+                # Modify the main dataframe, adding the found values.
+                df.at[record_index, 'key'] = key
+                df.at[record_index, 'chords'] = chords
+                # record['key'] = key
+                # record['chords'] = chords
+                break  # We successfully got the data for one of the search results. Move on to the next record.
+            except TimeoutException:
+                log('Timed out.')
+            except Exception:
+                logging.exception('Exception getting song data:')
+        if 'key' not in record or 'chords' not in record:
+            log('All {} results tried for {}: {}. Moving to next song without adding columns.'
+                .format(len(result_elements), record['artist'], record['song']))
+
+# Flatten the dataframe to a single level again, remove the temporary added 'year' column, and export to CSV.
+log('\nExporting CSV file with added "key" and "chords" columns to {}'.format(args.output))
+df.drop(columns=['year']).to_csv(args.output, header=True, index=False)
 
 log('Closing Chrome driver...')
 driver.quit()
